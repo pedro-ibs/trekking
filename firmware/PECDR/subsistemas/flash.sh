24ea71a5ca845dcffd84646fbef5f51446deac59
Pedro Igor <pedro.igor.ifsp@gmail.com>
2024-07-08 11:57:38 -0300

    removendo arrumando arquivos

--- firmware/PECDR/subsistemas/flash.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-scp build/RPI_PICO_SDK.elf tars@voyager.local:rpi_pico.elf
-ssh tars@voyager.local 'openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program rpi_pico.elf verify reset exit"'
